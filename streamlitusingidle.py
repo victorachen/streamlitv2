81b544a8c140d6b3b6ddace72e123ef704b0a524
Victor <vchen2120@gmail.com>
2022-10-06 18:42:27 -0700

    hoping this works?

--- streamlitusingidle.py
+++ streamlitusingidle.py
@@ -15,16 +15,6 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-##db.collection('persons').add({'name':'Jeff','age':40})
-db.collection('Vacancy').document('just_rented').update({'Unit':'(1/1)-$1650'})
-
-
-# Goal: work on getting firebase data and printing it
-##specificresult = db.collection('persons').document("p1").get()
-##if specificresult.exists:
-##    print(specificresult.to_dict())
-##
-
 
 entire_collection = db.collection('Vacancy').get()
 
@@ -47,12 +37,11 @@ for i in d:
 # unit_turns
 
 # outputs the entire text output (for streamlit to display)
-def text_output():
-    for i in d:
-        line_item = i + ": " + d[i]
-        st.write(line_item)
-    return None
-
+# def text_output():
+#     for i in d:
+#         line_item = i + ": " + d[i]
+#         st.write(line_item)
+#     return None
 
 # commenting off for now
 ##for unit_type in entire_collection:
@@ -66,19 +55,13 @@ def text_output():
 ##print(s)
 
 text_output()
-st.markdown("""Hello
-
-This is a cheat sheet built with the most common charts that I use in the data analysis for the projects.
-
-The idea is to have a quick and useful sheet where anyone can copy and past the code (1 line command usually) to generate the charts below.
-""")
 
 L = ['a', 'b', 'c', 'd']
 s = """ """
 for i in L:
     s += i + "\n"
 st.markdown(s)
-st.write('faster flow through github baby!')
+
 
 #STARTING THE REAL CODE BABY
 
@@ -91,14 +74,16 @@ no_status = db.collection('Vacancy').where("type", "==", "no_status").get()
 #Helper: replaces all "_" with " "
 def format(key,value):
     return key.replace('_',' ')+value
+
 def format2(dic):
     s = ""
     for i in dic:
-        s += i + ", "
+        if i!='type':
+            s += format(i,"") + ", "
     return s
 
-st.write('Rent Ready:')
-st.write('-  -  -  -  -  -  -  -  -  -  -')
+st.title('Rent Ready:')
+# st.write('-  -  -  -  -  -  -  -  -  -  -')
 for doc in rent_ready:
     d = doc.to_dict()
     for i in d:
@@ -122,14 +107,15 @@ st.write('Under Construction:')
 st.write('-  -  -  -  -  -  -  -  -  -  -')
 for doc in under_construction:
     d = doc.to_dict()
-    string = format2(d)
-    st.write(string)
+string = format2(d)
+st.write(string)
+
 st.write('No Status:')
 st.write('-  -  -  -  -  -  -  -  -  -  -')
 for doc in no_status:
     d = doc.to_dict()
-    string = format2(d)
-    st.write(string)
+string = format2(d)
+st.write(string)
 ##C:\\Users\\Lenovo\\anaconda3\\envs\\streamlit
 
 
